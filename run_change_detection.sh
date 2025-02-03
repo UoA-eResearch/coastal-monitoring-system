3ce8f8f573545a21357783370b82cd8fd6482676
coastal-monitor <ubuntu@monitoring>
2025-02-03 01:16:20 +0000

    auto update

--- run_change_detection.sh
+++ run_change_detection.sh
@@ -2,7 +2,7 @@
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate coastal-monitoring
 git pull
-./download_images.py
+#./download_images.py
 ./process_change_detection.py
 jupyter nbconvert --to notebook --execute --inplace apply_tidal_correction.ipynb calc_linear_trends.ipynb 
 git commit -am 'auto update' --author="coastal-monitor <ubuntu@monitoring>"
